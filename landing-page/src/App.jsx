e3e3def9c1a49e5dc77c4c166950b3e181c364fa
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-04-03 14:08:54 -0300

    App routes

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Shop from "./pages/Shop/Shop";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ landing-page/src/pages/Shop/Shop.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Shop.css";
+
+function Shop() {
+  return <div>Shop</div>;
+}
+
+export default Shop;
