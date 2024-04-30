f1cd20d423c5d939c3150ebf7a72d8cc25421c30
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-04-30 19:00:17 -0300

    new layout done

--- landing-page/src/components/Home/Siders/Sider-settings-navbar.jsx
+++ landing-page/src/components/Home/Siders/Sider-settings-navbar.jsx
@@ -15,25 +15,35 @@ function SiderSettingsNavbar() {
   if (logOutLoading) {
     return <LoadingScreen />;
   }
-  
+
   return (
     <nav className="sider-menu-navbar_container">
       <ul>
-        <li>
-          <GoGear className="sider-menu-navbar_icons" />
-        </li>
-        <li>
-          <TfiWorld className="sider-menu-navbar_icons" />
-        </li>
-        <li>
-          <TfiLayoutCtaBtnLeft className="sider-menu-navbar_icons" />
-        </li>
-        <li>
-          <BsShop className="sider-menu-navbar_icons" />
-        </li>
-        <li onClick={handleLogOut}>
-          <CiLogout className="sider-menu-navbar_icons sider-menu-navbar_icon-logout" />
-        </li>
+        <div>
+          <li>
+            <GoGear className="sider-menu-navbar_icons" />
+          </li>
+        </div>
+        <div>
+          <li>
+            <TfiWorld className="sider-menu-navbar_icons" />
+          </li>
+        </div>
+        <div>
+          <li>
+            <TfiLayoutCtaBtnLeft className="sider-menu-navbar_icons" />
+          </li>
+        </div>
+        <div>
+          <li>
+            <BsShop className="sider-menu-navbar_icons" />
+          </li>
+        </div>
+        <div>
+          <li onClick={handleLogOut}>
+            <CiLogout className="sider-menu-navbar_icons sider-menu-navbar_icon-logout" />
+          </li>
+        </div>
       </ul>
     </nav>
   );
--- landing-page/src/components/Home/Siders/Sider-settings.jsx
+++ landing-page/src/components/Home/Siders/Sider-settings.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import LoadingScreen from "../../Common/Spinner/LoadingScreen";
 import SiderSettingsNavbar from "./Sider-settings-navbar";
-import useLogOut from "../../../hooks/useLogOut";
 import { FaWhatsapp } from "react-icons/fa";
 import "./Sider-settings.css";
 
 function SiderSettings() {
-  const { handleLogOut, logOutLoading } = useLogOut();
-
   return (
     <div className="sider-settings_container">
       <div className="sider-settings">
         <SiderSettingsNavbar />
-        <a href="https://wa.me/+5492281378525" target="_blank" className="sider-settings_whatsapp-icon">
-          <FaWhatsapp className="wa-icon"/>
+        <a
+          href="https://wa.me/+5492281378525"
+          target="_blank"
+          className="sider-settings_whatsapp-icon"
+        >
+          <FaWhatsapp className="wa-icon" />
         </a>
       </div>
     </div>
