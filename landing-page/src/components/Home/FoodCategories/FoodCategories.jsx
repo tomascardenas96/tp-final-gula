c59cb48b76606ccccf820a0e9b943fd7414a6a61
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-04-30 14:43:51 -0300

    layout done

--- landing-page/src/components/Home/FoodCategories/FoodCategories.jsx
+++ landing-page/src/components/Home/FoodCategories/FoodCategories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CategoryCard from "./CategoryCard";
 import useGetAllCategories from "../../../hooks/useGetAllCategories";
+import Spinner from "../../Common/Spinner/Spinner.jsx"
 import "./FoodCategories.css";
 
 function FoodCategories() {
@@ -12,15 +13,21 @@ function FoodCategories() {
   return (
     <section className="food-categories_container">
       <h1>Categorias</h1>
-      <div className="food-categories_cards">
-        {categories.map((category, index) => (
-          <CategoryCard
-            key={category.categoryId}
-            icon={categoryIcons[index]}
-            title={category.description}
-          />
-        ))}
-      </div>
+      {!categoriesLoading ? (
+        <div className="food-categories_cards">
+          {categories.map((category, index) => (
+            <CategoryCard
+              key={category.categoryId}
+              icon={categoryIcons[index]}
+              title={category.description}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="food-categories_spinner-container">
+          <Spinner className="food-categories_spinner"/>
+        </div>
+      )}
     </section>
   );
 }
