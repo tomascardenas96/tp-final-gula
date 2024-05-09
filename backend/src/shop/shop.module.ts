b11bde453b3fe40cf336dd3187e5eea4bd431ce1
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-05-08 21:51:17 -0300

    .

--- backend/src/shop/shop.module.ts
+++ backend/src/shop/shop.module.ts
@@ -3,9 +3,10 @@ import { ShopService } from './shop.service';
 import { ShopController } from './shop.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Shop } from './entities/shop.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Shop])],
+  imports: [TypeOrmModule.forFeature([Shop]), UserModule],
   controllers: [ShopController],
   providers: [ShopService],
 })
--- backend/src/shop/shop.service.ts
+++ backend/src/shop/shop.service.ts
@@ -8,11 +8,14 @@ import { UpdateShopDto } from './dto/update-shop.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Shop } from './entities/shop.entity';
 import { ILike, Repository } from 'typeorm';
+import { ActiveUserInterface } from 'src/common/interface/active-user.interface';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class ShopService {
   constructor(
     @InjectRepository(Shop) private readonly shopRepository: Repository<Shop>,
+    private readonly userService: UserService,
   ) {}
 
   create(createShopDto: CreateShopDto) {
@@ -27,10 +30,19 @@ export class ShopService {
     }
   }
 
+  async getShopsByActiveUser(user: ActiveUserInterface) {
+    try {
+      const activeUser = await this.userService.findByEmail(user.email);
+      return this.shopRepository.find({ where: { user: activeUser } });
+    } catch (err) {
+      throw new BadGatewayException({ errorMessage: err });
+    }
+  }
+
   findShopByQuery(shop: string) {
     try {
       return this.shopRepository.find({
-        where: { name: ILike(`%${shop}%`) }
+        where: { name: ILike(`%${shop}%`) },
       });
     } catch (err) {
       throw new ForbiddenException(err);
