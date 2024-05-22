99f1f1467ea8603546b016c1f327b13a8a2534b6
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-05-22 20:07:56 -0300

    Installed serve-static library, and configured in main module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import * as bodyParser from 'body-parser';
 //Para poder utilizar las variables de entorno.
 import { config } from 'dotenv';
 config();
@@ -15,6 +16,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  //Para modificar el limite permitido de carga de archivos por parte del usuario.
+  app.use(bodyParser.json({ limit: '10000mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10000mb', extended: true }));
   await app.listen(3070);
 }
 bootstrap();
