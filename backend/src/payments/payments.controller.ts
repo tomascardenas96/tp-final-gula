441795f34bdfed83873efbf649262f6c358e58ef
Tomás Cárdenas <tomicardenas96@gmail.com>
2024-07-02 16:19:22 -0300

    Fixed bugs to initialize the project

--- backend/src/payments/payments.controller.ts
+++ backend/src/payments/payments.controller.ts
@@ -9,8 +9,6 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { ActiveUser } from 'src/common/decorator/active-user.decorator';
-import { ActiveUserInterface } from 'src/common/interface/active-user.interface';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
 import { FoodOnCart } from 'src/food_on_cart/entities/food_on_cart.entity';
 
@@ -20,10 +18,7 @@ export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post('create-preference')
-  async createPreference(
-    @ActiveUser() activeUser: ActiveUserInterface,
-    @Body() foodOnCart: FoodOnCart[],
-  ) {
-    return await this.paymentsService.createPreference(activeUser, foodOnCart);
+  async createPreference(@Body() foodOnCart: FoodOnCart[]) {
+    return await this.paymentsService.createPreference(foodOnCart);
   }
 }
